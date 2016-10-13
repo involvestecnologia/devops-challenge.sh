ef0e7d946e4c5b85e298a695624691adf3e8fe8a
Brunno de Almeida Santos <balmeida.santos@hotmail.com>
2016-10-13 20:19:37 -0300

    Update server.js
    alterando ip para localhost

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const MongoClient = require('mongodb').MongoClient
 
 var db
 
-MongoClient.connect('mongodb://127.0.0.1:27017/devops-test', function(err, database) {
+MongoClient.connect('mongodb://localhost:27017/devops-test', function(err, database) {
   if (err) return console.log(err)
   db = database
   app.listen(3000, function(){
