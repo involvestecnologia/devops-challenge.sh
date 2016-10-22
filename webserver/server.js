e5c566e216178c4964211122e935b7ca6197a48d
zehamarall <zehamarall@gmail.com>
2016-10-22 11:51:21 -0200

    Criação dos botões de replace e clean com implematação do backend.

--- webserver/server.js
+++ webserver/server.js
@@ -15,7 +15,8 @@ if ( DOCKER_DB ) {
 }
 
 MongoClient.connect(MONGO_DB, (err, database) => {
-  if (err) return console.log(err)
+  if (err) 
+    return console.log(err)
   db = database
   app.listen(process.env.WEB_APP_PORT || 3000, () => {
     console.log('listening on 3000')
@@ -27,18 +28,43 @@ app.use(bodyParser.urlencoded({extended: true}))
 app.use(bodyParser.json())
 app.use(express.static('public'))
 
-app.get('/', (req, res) => {
+app.get('/', (req, res) => { 
   db.collection('quotes').find().toArray((err, result) => {
-    if (err) return console.log(err)
+    if (err) 
+      return console.log(err)
     res.render('index.ejs', {quotes: result})
   })
 })
 
 app.post('/quotes', (req, res) => {
-  db.collection('quotes').save(req.body, (err, result) => {
+  db.collection('quotes').save(req.body,(err, result) => {
     if (err) return console.log(err)
     console.log('saved to database')
     res.redirect('/')
   })
 })
 
+app.delete('/quotes', (req, res) => {
+  db.collection('quotes').remove({}, (err, result) => {
+    if (err){
+      console.log(err)
+      return res.send(500, err)
+    }
+    console.log('removed to database')
+    res.redirect('/')
+  })
+})
+
+app.put('/quotes',(req, res) => {
+  var collection = db.collection('quotes');
+  collection.find({quote: /WINDOWS/i }).forEach((e,i) => {
+    console.log("OBJ = ", e);
+    e.quote=e.quote.replace(/WINDOWS/ig, "Linux is better");
+    db.collection('quotes').save(e);
+    },(err, result) =>{
+      if (err) return console.log(err)
+      console.log("quote replaced to database")
+      res.redirect('/')
+      res.send(result)
+    })
+})
